5418e968a9f27884db61de5475b9daee3326ebbc
Manish Kumar <manishkumarg7394@gmail.com>
2026-07-19 15:35:03 +0530

    Create Advanced_SQL.sql

---
+++ Advanced_SQL.sql
@@ -0,0 +1,149 @@
+-- Section 8 — Advanced SQL
+
+-- Q40
+-- Rank players within each team based on tournament rating.
+-- (Window Function)
+SELECT 
+	player_name,
+	team,
+	round(
+	avg(tournament_rating)
+	::numeric,2) as total_rating,
+	rank()over (
+	partition by team 
+	order by avg(tournament_rating) desc
+	) as rnk
+	
+FROM fifa_player
+group by 
+	player_name,
+	team
+ORDER BY  rnk, team
+
+
+-- Q41
+-- Find the second-best player from every team.
+-- (Dense Rank)
+with cte as (
+select 
+	player_name,
+	team,
+	sum(total_goals_tournament) as total_goals,
+	round(avg(tournament_rating)::numeric,2) as avg_rating,
+	dense_rank()over (partition by team 
+		order by sum(total_goals_tournament) desc) as rnk
+FROM fifa_player
+GROUP BY player_name, team
+)
+
+select * from cte
+where rnk = 2
+order by team 
+
+
+-- Q42
+-- Find the top-rated player for every position.
+with CTE as (
+SELECT 
+	player_name,
+	team,
+	position,
+	
+	round(
+	avg(player_rating)
+	::numeric,2) as avg_rating,
+	
+	dense_rank() over(partition by position
+		order by avg(player_rating) desc)as rnk
+		
+FROM fifa_player
+GROUP BY  player_name, team, position
+)
+SELECT * FROM CTE
+WHERE rnk  = 1
+
+
+-- Q43
+-- Calculate cumulative tournament goals by match date.
+-- (Running Total)
+with daily_goals as (
+SELECT 
+	match_date,
+	sum(total_goals_tournament) as daily_goals
+FROM fifa_player
+group by match_date
+)
+select 
+	match_date,
+	daily_goals,
+	sum(daily_goals) over(order by match_date ) as totol_running
+from daily_goals
+ORDER BY match_date
+
+-- Q44
+-- Compare each player's rating with the previous match.
+-- (LAG)
+SELECT
+    player_name,
+    team,
+    match_date,
+    player_rating,
+
+    LAG(player_rating) OVER (
+        PARTITION BY player_name
+        ORDER BY match_date
+    ) AS previous_rating
+
+FROM fifa_player
+
+ORDER BY
+    player_name,
+    match_date;
+
+
+-- Did the player improve?
+
+SELECT
+    player_name,
+    match_date,
+    player_rating,
+
+    LAG(player_rating) OVER (
+        PARTITION BY player_name
+        ORDER BY match_date
+    ) AS previous_rating,
+
+    ROUND(
+        player_rating -
+        LAG(player_rating) OVER (
+            PARTITION BY player_name
+            ORDER BY match_date
+        ),
+        2
+    ) AS rating_change
+
+FROM fifa_player
+
+-- Q45
+-- Compare each player's rating with the next match.
+-- (LEAD)
+SELECT
+    player_name,
+    match_date,
+    player_rating,
+
+    LEAD(player_rating) OVER (
+        PARTITION BY player_name
+        ORDER BY match_date
+    ) AS Next_rating,
+
+    ROUND(
+        player_rating -
+        LEAD(player_rating) OVER (
+            PARTITION BY player_name
+            ORDER BY match_date
+        ),
+        2
+    ) AS rating_change
+
+FROM fifa_player;
